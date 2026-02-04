a6a082ba0c25bfe759cd3acd277d36e3649feba4
Donovan Dicks <donovan.dicks@outlook.com>
2026-02-03 23:15:00 -0500

    fix: More robust curdir check

--- src/tools/utils/checkCurrentDirectory.ts
+++ src/tools/utils/checkCurrentDirectory.ts
@@ -8,5 +8,10 @@
  * @returns True if the path is located within the current working directory.
  */
 export function checkPathWithinCurrentDir(path: string): boolean {
-  return path.startsWith(Deno.cwd()) || path.startsWith(".");
+  if (path.startsWith(Deno.cwd()) || path.startsWith(".")) return true;
+
+  const cwdPaths = [...Deno.readDirSync(Deno.cwd()).map((entry) => entry.name)];
+  if (cwdPaths.some((cwdPath) => path.startsWith(cwdPath))) return true;
+
+  return false;
 }
--- tests/unit/checkCurrentDirectory.test.ts
+++ tests/unit/checkCurrentDirectory.test.ts
@@ -1,5 +1,6 @@
 import { checkPathWithinCurrentDir } from "../../src/tools/utils/checkCurrentDirectory.ts";
 import { assertEquals } from "@std/assert";
+import { stub } from "@std/testing/mock";
 
 Deno.test({
   name: "checkPathWithinCurrentDir",
@@ -25,7 +26,25 @@ Deno.test({
         path: `${Deno.cwd()}/path`,
         expected: true,
       },
+      {
+        path: "src",
+        expected: true,
+      },
+      {
+        path: "other",
+        expected: false,
+      },
     ];
+
+    const mockDirContents: Deno.DirEntry[] = [
+      { name: "main.ts", isFile: true, isDirectory: false, isSymlink: false },
+      { name: "src", isFile: false, isDirectory: true, isSymlink: false },
+      { name: "test", isFile: false, isDirectory: true, isSymlink: false },
+    ];
+
+    using _ = stub(Deno, "readDirSync", () => {
+      return mockDirContents[Symbol.iterator]();
+    });
 
     for (const { path, expected } of cases) {
       const actual = checkPathWithinCurrentDir(path);
