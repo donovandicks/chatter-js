c6eff609954349c713aa3ef9a4c1641a3a925562
Donovan Dicks <donovan.dicks@outlook.com>
2026-02-03 22:30:38 -0500

    feat: support certain CLI flags

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import "@std/dotenv/load";
+import { parseArgs } from "@std/cli";
 
 import { render } from "ink";
 import { Chat } from "./components/Chat.tsx";
 import { ChatService } from "./services/chatService.ts";
 import { loadSettings } from "./config/loader.ts";
 import { ChatterSettings } from "./types/config.ts";
+
+type Args = {
+  configPath?: string;
+  noninteractivePrompt?: string;
+};
+
+function parseCliArgs(): Args {
+  const args = parseArgs(Deno.args, {
+    "string": ["config"],
+  });
+
+  const positionalArgument = args._[0];
+
+  return {
+    configPath: args.config,
+    noninteractivePrompt: positionalArgument ? `${positionalArgument}` : undefined,
+  };
+}
 
 async function handleNoninteractive(settings: ChatterSettings, prompt: string) {
   const chatService = new ChatService({
@@ -26,19 +45,19 @@ function handleInteractive(settings: ChatterSettings) {
 }
 
 async function main() {
+  const args = parseCliArgs();
   let settings: ChatterSettings | undefined = undefined;
 
   try {
-    settings = loadSettings();
+    settings = loadSettings(args.configPath);
   } catch (error) {
     console.error("%cFailed to load settings:", "color: red");
     console.error(`%c${(error as Error).message}`, "color: red");
     Deno.exit(1);
   }
 
-  const prompt = Deno.args[0];
-  if (prompt) {
-    await handleNoninteractive(settings, prompt);
+  if (args.noninteractivePrompt) {
+    await handleNoninteractive(settings, args.noninteractivePrompt);
     Deno.exit(0);
   }
 
