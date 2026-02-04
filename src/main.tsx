0c1e55d6ed4e1523a989c4e58890f2ca0251c1b5
Donovan Dicks <donovan.dicks@outlook.com>
2026-02-03 21:55:55 -0500

    refactor: breakout main functions

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,37 @@
 import "@std/dotenv/load";
+
 import { render } from "ink";
 import { Chat } from "./components/Chat.tsx";
 import { ChatService } from "./services/chatService.ts";
 
-if (import.meta.main) {
-  const prompt = Deno.args[0];
-  if (prompt) {
-    const chatService = new ChatService({
-      onEvent: (event) => {
-        if (event.displayText) {
-          console.log(`[${event.type}] ${event.displayText}`);
-        }
-      },
-    });
-    const response = await chatService.sendMessage(prompt);
-    console.log(response);
-    Deno.exit(0);
-  }
+async function handleNoninteractive(prompt: string) {
+  const chatService = new ChatService({
+    onEvent: (event) => {
+      if (event.displayText) {
+        console.log(`[${event.type}] ${event.displayText}`);
+      }
+    },
+  });
+  const response = await chatService.sendMessage(prompt);
+  console.log(response);
+}
 
+function handleInteractive() {
   // Clear screen to give a full-screen app feel
   console.clear();
   render(<Chat />);
+}
+
+async function main() {
+  const prompt = Deno.args[0];
+  if (prompt) {
+    await handleNoninteractive(prompt);
+    Deno.exit(0);
+  }
+
+  handleInteractive();
+}
+
+if (import.meta.main) {
+  await main();
 }
